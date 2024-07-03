e54bf7850a9d8f259acc54669c97231654c610c6
hyehyunj <developh620@gmail.com>
2024-07-03 14:44:30 +0900

    다국어지원을 위한 파일생성

--- app/src/main/java/com/example/menujo/MainActivity.kt
+++ app/src/main/java/com/example/menujo/MainActivity.kt
@@ -2,11 +2,7 @@ package com.example.menujo
 
 import android.os.Bundle
 import android.widget.EditText
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,8 +10,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
     //뷰연결
-    val etId = findViewById<EditText>(R.id.et_signIn_id)
-    val etPwd = findViewById<EditText>(R.id.et_signIn_pwd)
+    val etId = findViewById<EditText>(R.id.et_signin_id)
+    val etPwd = findViewById<EditText>(R.id.et_signin_pwd)
 
 
 
