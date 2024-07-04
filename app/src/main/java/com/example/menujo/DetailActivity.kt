f1e7e0325d924dd3ce6bab3469fde321e1c43eb3
Anna <a700hui@gmail.com>
2024-07-04 18:44:23 +0900

    feat : 디테일 페이지 : 리스트 레이아웃, 스트링 메인 페이지 : snackbar 랜덤 메시지 추가, 이미지 버튼 액션 추가

--- app/src/main/java/com/example/menujo/DetailActivity.kt
+++ app/src/main/java/com/example/menujo/DetailActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.ListView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -23,6 +24,16 @@ class DetailActivity : AppCompatActivity() {
         }
         initToolBar()
     }
+
+
+
+
+
+
+
+
+
+
 
     //date class
 
--- app/src/main/java/com/example/menujo/MainPageActivity.kt
+++ app/src/main/java/com/example/menujo/MainPageActivity.kt
@@ -15,6 +15,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.ViewCompat.*
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.snackbar.Snackbar
+import kotlin.random.Random
 
 class MainPageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,18 +29,46 @@ class MainPageActivity : AppCompatActivity() {
         }
         initToolBar()
         initNavigate()
-        snackbar()
+        showsnackbar()
+
+        findViewById<ImageButton>(R.id.ib_koreanfood).setOnClickListener {
+            initImageButton(it)
+        }
+        findViewById<ImageButton>(R.id.ib_chinesefood).setOnClickListener {
+            initImageButton(it)
+        }
+        findViewById<ImageButton>(R.id.ib_westernfood).setOnClickListener {
+            initImageButton(it)
+        }
+        findViewById<ImageButton>(R.id.ib_japanesefood).setOnClickListener {
+            initImageButton(it)
+        }
     }
 
-    private fun snackbar()
+    private fun showsnackbar()
     {
         val snackbarBtn = findViewById<Button>(R.id.btn_snackbar)
         snackbarBtn.setOnClickListener {
-            val snack1 = Snackbar.make(it, "눌러보삼",Snackbar.LENGTH_LONG)
-                .setAction("확인")
+
+            val random = Random
+            val num = random.nextInt(5)
+
+            val message = when(num)
+            {
+                0 -> getString(R.string.main_snackbar_not_full)
+                1 -> getString(R.string.main_snackbar_not_curious)
+                2 -> getString(R.string.main_snackbar_dawn)
+                3 -> getString(R.string.main_snackbar_diet)
+                else -> getString(R.string.main_snackbar_bbb)
+
+            }
+
+            val snack1 = Snackbar.make(it, message,Snackbar.LENGTH_LONG)
+                .setAction("종료하세요~")
             {
                 finish()
             }
+
             snack1.setTextColor(Color.BLACK)
             snack1.setBackgroundTint(Color.WHITE)
             snack1.animationMode = Snackbar.ANIMATION_MODE_FADE //스낵바가 아래에서 위로 올라오는 것과 같은 애니메이션 모드 변경
@@ -47,17 +76,8 @@ class MainPageActivity : AppCompatActivity() {
         }
     }
 
-
-
     private fun initImageButton(view:View)
     {
-//
-//        val koreanFood = findViewById<ImageButton>(R.id.ib_koreanfood)
-//        val chineseFood = findViewById<ImageButton>(R.id.ib_chinesefood)
-//        val westernFood = findViewById<ImageButton>(R.id.ib_westernfood)
-//        val japaneseFood = findViewById<ImageButton>(R.id.ib_japanesefood)
-//
-
         when(view.getId())
         {
             R.id.ib_koreanfood ->
@@ -90,23 +110,33 @@ class MainPageActivity : AppCompatActivity() {
         }
     }
 
-
-
     private fun initToolBar() {
         val accountIcon = findViewById<ImageView>(R.id.iv_right_icon)
         val userName = findViewById<TextView>(R.id.tv_user_name)
         val loginBtn = findViewById<Button>(R.id.btn_login)
 
-        // 로그인 확인 조건문(임시)
+        //회원가입에서 user이름 가져오는 임시 코드 !!
+        val user_Name = intent.getStringExtra("userNameKey") ?: "????"
+        userName.text = getString(R.string.main_sir, user_Name)
+
+
+        // 로그인 확인 조건문(임시) - 송주영님
         if(false) {
-            loginBtn.setVisibility(View.GONE);
+            loginBtn.setVisibility(View.GONE)
         }else{
-            accountIcon.setVisibility(View.GONE)
+            accountIcon.setVisibility(View.VISIBLE)
             userName.setVisibility(View.GONE)
         }
 
         loginBtn.setOnClickListener {
             val intent = Intent(this, SignInActivity::class.java)
+            startActivity(intent)
+        }
+
+        //마이페이지 클릭하면 유저 아이디를 마이페이지로 전달
+        accountIcon.setOnClickListener{
+            val intent = Intent(this, MyPageActivity::class.java)
+            intent.putExtra("userID",user_Name)
             startActivity(intent)
         }
     }
