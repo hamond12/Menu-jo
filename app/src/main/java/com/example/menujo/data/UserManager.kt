6d6f9486a3e4c89a1b391ab587ed27262a0695d0
seojw0124 <wjddn2000124@gmail.com>
2024-07-05 18:57:04 +0900

    feat: 마이페이지, 디테일 페이지 툴바 유저 정보 표시

--- app/src/main/java/com/example/menujo/data/UserManager.kt
+++ app/src/main/java/com/example/menujo/data/UserManager.kt
@@ -2,8 +2,6 @@ package com.example.menujo.data
 
 object UserManager {
     private var userList = initUserData()
-    var currentUser: UserInfo = UserInfo("", "", "", "", emptyList())
-        private set
 
     private fun initUserData(): MutableList<UserInfo> {
         return mutableListOf(
@@ -47,14 +45,9 @@ object UserManager {
 
     fun saveUser(user: UserInfo) {
         userList.add(user)
-        currentUser = user
     }
 
     fun getUser(userId: String): UserInfo? {
         return userList.find { it.userId == userId }
-    }
-
-    fun getUserByName(userName: String): UserInfo? {
-        return userList.find { it.userName == userName }
     }
 }
